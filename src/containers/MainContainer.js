347ddb418618a594dadbcd9492a9e0f71cf65f76
Brianna Foley <brianna.foley87@gmail.com>
2022-11-18 12:40:43 -0600

    initial commit

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,14 @@
+import Button from 'react-bootstrap/Button'
+
+<div id="drum-machine">
+  <div id="display"></div>
+  <Button id="" className="drum-pad">Q</Button>
+  <Button id="" className="drum-pad">W</Button>
+  <Button id="" className="drum-pad">E</Button>
+  <Button id="" className="drum-pad">A</Button>
+  <Button id="" className="drum-pad">S</Button>
+  <Button id="" className="drum-pad">D</Button>
+  <Button id="" className="drum-pad">Z</Button>
+  <Button id="" className="drum-pad">X</Button>
+  <Button id="" className="drum-pad">C</Button>
+</div>
